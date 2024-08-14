6c0a269d5c351bc5210c263c1b43879c889681f3
millimesi <millionmesi1@gmail.com>
2024-08-14 19:19:12 +0300

    Remove .env file from repository

--- utils/midlewares.js
+++ utils/midlewares.js
@@ -8,11 +8,11 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (!token) return res.status(401).json({ error: 'Access Denied: No Token Provided' });
+    if (!token) return res.status(401).json({ error: 'Access needs authorization' });
     
     // Verify the token
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.status(403).json({ error: 'Invalid Token' });
+        if (err) return res.status(403).json({ error: ' Access unauthorized' });
 
         req.user = user; // Attach the decoded token payload to the request object
         next(); // Pass control to the next middleware function
